c52efbf350fad3a1f839647eb7db3c00aaa471ae
Brett Esler <brett.esler@teamunicorn.net>
2024-03-08 14:02:21 +1100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -8,11 +8,17 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def txdev(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('txdev')
     
-    rsp = readfile("metadata.json")
+    rsp = "{}"
 
     if "/metadata?mode=terminology" in req.url:
-        rsp = "{}"
-
+        rsp = readfile("terminology.json")
+    elif "/metadata" in req.url:
+        rsp = readfile("metadata.json")
+    elif  "common-languages-australia-1" in req.url or "unitsofmeasure.org" in req.url:
+        rsp = readfile("tx-reg.json")
+    else
+        rsp = readfile("tx-reg-csiro.json")
+        
     return func.HttpResponse(
       rsp,
       headers = {'Content-Type' : 'application/json'},
